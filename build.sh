b43b8674c9d9a5e9763dc19fdf4233afe5e4531d
Manlio García <quarkex@gmail.com>
2016-12-16 20:53:42 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#ensure that our working dir is empty
+if [[ ! -d _site ]]; then
+    mkdir _site;
+else
+    if [[ -f _site ]]; then
+        mv _site _site.bak;
+        mkdir _site;
+    fi
+    rm -r _site/*;
+fi
+
+# grabs all releant files in _sass folder and builds a single, minified css
+echo "" > _sass/_sass_imports.scss;
+if [[ -f _sass/_variables.scss ]]; then echo "@import '_variables.scss';" > _sass/_sass_imports.scss; fi
+for folder in _sass/_{'tag','class','id'}; do 
+    if [[ -d $folder ]]; then
+        for i in $folder/*.scss; do
+            if [[ -f $i ]]; then
+                echo "@import '${i##_sass/}';" >> _sass/_sass_imports.scss;
+            fi
+        done;
+    fi
+done
+sass --load-path _sass _sass/index.scss --style compressed > style.min.css;
+
+# grabs all files wich don't start with an underscore or in our blacklist
+for i in $(find . -type f | grep -v -e "/_" -e ".sass-cache" -e '.git' -e '.gitignore'); do
+    if [[ ${i##./} == ${0##./} ]]; then continue; fi
+    item="${i##./}";
+    if [[ ! ${item%/*} == ${i##*/} ]]; then
+        mkdir -p _site/${item%/*};
+    fi
+    mime="$(mimetype -bi $item)";
+    case $mime in
+        "application/javascript")
+            if [[ ${item##*.min.} == "js" ]]; then
+                cp "$item" _site/"$item"
+            else
+                uglifyjs "$item" -o _site/"$item" -c
+            fi
+            ;;
+        "text/css")
+            if [[ ${item##*.min.} == "css" ]]; then
+                cp "$item" _site/"$item"
+            else
+                sass "$item" --style compressed > _site/"${item%.css}.min.css"
+            fi
+            ;;
+        "text/html")
+            # Usually is not worth adding an HTML compressor tool, but just if necesary, it should be here
+            cp "$item" _site/"$item"
+            ;;
+        *)
+            cp "$item" _site/"$item"
+            ;;
+    esac
+done
+
---
+++ js/aronaTravelApp.js
@@ -0,0 +1,128 @@
+/**
+ * aronaTravelApp.js
+ * Initializes Arona.travel app in Angular.js
+ *
+ * Author: Manlio Joaquín García González (manliojoaquin@gmail.com)
+ *
+ * This file contains the initializator, values and methods for the app.
+ * Provides behaviour to set localization based on url location, build
+ * navigation trees and import panels and subpanels as required.
+ *
+ * Once loaded, you may use these variables and methods in your Angular app:
+ *
+ * - {{ translate( string, [prefix] ) }}
+ *
+ *   Fetch a localized alternative from the internal dictionary, or the string
+ *   if none is found.
+ *
+ * - {{ page }}
+ *
+ *   Returns the internal page object, with variables like title or dictionary
+ *   The page object act as a global scope variables holder.
+ *
+ * - {{ lang() }}
+ *
+ *   Returns the current language based on the url location as a string
+ *
+ * - {{ path() }}
+ *
+ *   Returns the current angular path location as a string
+ *
+ * - {{ nav }}
+ *
+ *   Returns an object with arrays of navigation links. They can also have more content.
+ *   E.G: {{ nav.left }} => [{"label":"AngularJS", "href":"http://angularjs.org", "content":[...]}]
+ *
+ * - {{ breadcrumbs }}
+ *
+ *   Returns an object with arrays of navigation links based on current location.
+ *   E.G: {{ breadcrumbs }} => [{"label":"home", "href":"#/"},{"label":"angular", "href":"#/angular"}]
+ */
+
+var app = angular.module("aronaTravelApp", ["ngRoute","ngResource","mm.foundation"]);
+
+app.value('page', {'title': "Arona.travel", "dictionary":{}});
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/:language", {
+        templateUrl : "assets/panels/main.htm",
+        controller: "aronaTravelCtrl"
+    })
+    .when("/:language/404", {
+        templateUrl : "assets/404.htm",
+        controller: "aronaTravelCtrl"
+    })
+    .when("/:language/eventos/:event", {
+        templateUrl : function(urlattr){
+            return '/assets/panels/eventos.htm';
+        },
+        controller: "aronaTravelCtrl"
+    })
+    .when("/:language/:panel*", {
+        templateUrl : function(urlattr){
+            return '/assets/panels/' + urlattr.panel + '.htm';
+        },
+        controller: "aronaTravelCtrl"
+    })
+    .when("/", {
+        redirectTo: "/en"
+    })
+    .otherwise({
+        redirectTo: function(urlattr){
+            return '/' + urlattr.language + '/404';
+        }
+    });
+});
+
+app.controller("aronaTravelCtrl", function($scope, $location, $routeParams, $resource, page) {
+
+    $scope.page = page;
+
+    $scope.lang = function (){ return $routeParams.language; };
+
+    $resource(function(){
+        if ( $scope.lang() == undefined ) return '/locales/en.json';
+        else return '/locales/' + $scope.lang() + '.json';
+    }(), {lang:$scope.lang()}).get(function(data){
+        for (var k in data){
+            if (data.hasOwnProperty(k)) {
+                page.dictionary[k] = data[k];
+            }
+        }
+    });
+
+    $scope.path = function (){ return $location.path(); };
+    $scope.breadcrumbs = function() {
+        var output = [];
+        var path = $location.path().substr(1).split('/');
+        var path_length = path.length;
+        for (var i = 0; i < path_length; i++){
+            var item = path[path.length - 1];
+            item = {'label': item, 'href': '#/' + path.join('/')};
+            if (i == 0) item.current = true;
+            if (i == (path_length - 1) ) item.label = "inicio";
+            output.push(item);
+            path.pop();
+        }
+        output.reverse();
+        return output;
+    };
+
+    $scope.nav = {};
+    $resource('/clockworks/fetch_navigation.json', {lang:$scope.lang()}).get(function(data){
+        $scope.nav = data;
+    });
+
+    $scope.translate = function(stringA, stringB = null) {
+        // this is to respect the less surprise directive. Prefixes should precede the target string
+        var string = stringB == null ? stringA : stringB;
+        var prefix = stringB == null ? null : stringA;
+
+        if ( page.dictionary.hasOwnProperty( prefix == null? string : prefix + string ) ){
+            string = page.dictionary[prefix == null ? string : prefix + string];
+        }
+        return string;
+    };
+
+});
