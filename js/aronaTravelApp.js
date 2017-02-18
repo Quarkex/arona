bf10d27145f9d8a1816a6fbc80343e4cad6d1523
Manlio García <quarkex@gmail.com>
2017-02-18 11:55:15 +0000

    removed underscore from breadcrumbs and configured colors

--- js/aronaTravelApp.js
+++ js/aronaTravelApp.js
@@ -452,13 +452,20 @@ app.config(function($routeProvider, tmhDynamicLocaleProvider, $animateProvider, 
 
     // By default, shades 500, 300 800 and A100 are used for primary and warn intentions, while A200, A100, A400 and A700 are used for accent.
     $mdThemingProvider.extendPalette('indigo', {
-        '500': '#15388a',
+        '400': '#15388a',
         'contrastDefaultColor': 'dark'
     });
     $mdThemingProvider.extendPalette('pink', {
-        'A100': '#d2251a',
+        '400': '#d2251a',
         'contrastDefaultColor': 'light'
     });
+    $mdThemingProvider.theme('default')
+        .primaryPalette('indigo', {
+            'default': '400', // by default use shade 400 from the pink palette for primary intentions
+            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+        })
 
 
     $routeProvider
