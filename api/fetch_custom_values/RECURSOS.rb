be89846750768e45c99cb7ace36d9131ed2edddf
Manlio García <quarkex@gmail.com>
2017-04-25 01:52:12 +0100

    added support for item resources in fetcher

--- api/fetch_custom_values/RECURSOS.rb
+++ api/fetch_custom_values/RECURSOS.rb
@@ -4,9 +4,10 @@ if @doc.has_key? 'COD' + @value then
     if doc_resources.is_a? String then
         doc_resources = doc_resources.split '&'
         doc_resources.map! { |i| i.to_i }
-    elsif doc_resources.is_a? Int then
+    elsif doc_resources.is_a? Integer then
         doc_resources = [doc_resources]
     end
+    doc_resources = [] if doc_resources == nil
     doc_resources.map! { |i|
         resource = $resources.find({ "IDIOMA": "es", "CODRECURSO": i, "MULTIIDIOMA": 1 }).sort("RECURSO" => -1).first
         resource = $resources.find({ "IDIOMA": @doc["IDIOMA"], "CODRECURSO": i }).sort("RECURSO" => -1).first if resource == nil
@@ -14,5 +15,11 @@ if @doc.has_key? 'COD' + @value then
     }
     doc_resources = nil if doc_resources.size == 0
 
-    @custom_value = doc_resources
+    availible_resources = []
+    doc_resources.each do | resource |
+        availible_resources.push(resource) if should_add(resource) == true
+    end if doc_resources != nil
+    availible_resources = nil if availible_resources.size == 0
+
+    @custom_value = availible_resources
 end
