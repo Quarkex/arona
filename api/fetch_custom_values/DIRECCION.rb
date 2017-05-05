975fa7af254e19461a247d8cb4c554afb3cb90a4
Manlio García <quarkex@gmail.com>
2017-05-05 11:18:36 +0100

    ignored dreaded “-1” value in DIRECCION

--- api/fetch_custom_values/DIRECCION.rb
+++ api/fetch_custom_values/DIRECCION.rb
@@ -2,7 +2,7 @@ address = ""
 chunks = [ "TIPO_VIA", "NOMBRE_VIA", "NUMERO", "BLOQUE", "PORTAL", "ESCALERA", "PLANTA", "PUERTA", "LOCAL", "CODIGO_POSTAL", "ZONA" ]
 
 chunks.each do | chunk |
-    if @doc.has_key? chunk and @doc[chunk] != nil then
+    if @doc.has_key? chunk and @doc[chunk] != nil and @doc[chunk] != -1 then
         chunk = chunk.to_s
         address = address + ", " if not address == "" unless chunk == "CODIGO_POSTAL" or chunk == "NOMBRE_VIA"
         address = address + " "  if chunk == "NOMBRE_VIA" unless address == ""
