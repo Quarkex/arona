03390b8b30c17e0fef69cb4fce4a9002f9f76fd0
Manlio García <quarkex@gmail.com>
2017-05-05 08:43:24 +0100

    dates are now returned as espected by the web

--- api/fetch_custom_values/F_INICIO_PUB.rb
+++ api/fetch_custom_values/F_INICIO_PUB.rb
@@ -1,1 +1,1 @@
-@custom_value = @doc[@value] if @doc[@value] != DateTime.parse("9999-12-30 00:00:00 UTC")
+@custom_value = @doc[@value].iso8601 if @doc[@value] != DateTime.parse("9999-12-30 00:00:00 UTC")
