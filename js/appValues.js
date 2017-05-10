2c593c12740a83b5aeb7cfe6ab5939d6d2ded4d9
Manlio García <quarkex@gmail.com>
2017-05-10 12:48:33 +0100

    moved app values to its own file

---
+++ js/appValues.js
@@ -0,0 +1,109 @@
+app.value('page', {
+    'title': "Arona.travel",
+    'panels':{}
+});
+
+app.value('constants', {
+    "SUBTIPO_PRINCIPAL": {
+        "apartamentos":                           "Apartamentos",
+        "hoteles":                                "Hoteles",
+        "pensiones":                              "Pensiones",
+        "ialojamiento_rural":                     "Alojamiento Rural",
+        "aparthoteles":                           "Aparthoteles"
+    },
+    "SUBTIPO": {
+        "panflets":                               "Folletos / Trípticos"
+    },
+    "CODCONTENIDO": {
+        "clima":                                  21687,
+        "compromiso_con_la_calidad":              21849,
+        "playa_de_las_vistas":                    22539,
+        "playa_de_las_vistas_accesible":          2106,
+        "playa_de_las_galletas":                  22569,
+        "puerto_de_las_galletas":                 489,
+        "plaza_de_arona":                         22543,
+        "la_conquista_de_canarias":               4180,
+        "los_cristianos":                         22573,
+        "otras_webcams":                          22585
+    },
+    'CODSUBTIPOCONT':{
+        "agencias_de_viaje":                      48,
+        "alojamiento_rural":                      15,
+        "alquiler_de_ayudas_tecnicas":            669,
+        "alquiler_de_vehiculos":                  47,
+        "apartahteles":                           659,
+        "apartamentos":                           40,
+        "arquitectura_tradicional":               490,
+        "arona_unica":                            517,
+        "arte_y_artesania":                       473,
+        "auditorios":                             596,
+        "biosphere":                              517,
+        "centros_civicos":                        466,
+        "centros_culturales":                     146,
+        "ciclismo":                               454,
+        "compras":                                157,
+        "costumbres_y_folclore":                  490,
+        "deporte_y_aventura":                     454,
+        "discotecas_y_pubs":                      249,
+        "espacios_naturales":                     327,
+        "especialidades_nauticas":                454,
+        "espectaculos":                           330,
+        "estadisticas":                           592,
+        "gimnasia_y_juegos_deportivos":           253,
+        "golf":                                   310,
+        "hipica":                                 454,
+        "historia_de_arona":                      329,
+        "hoteles":                                26,
+        "informacion_portuaria":                  524,
+        "instalaciones_deportivas":               321,
+        "lugares_de_interes":                     331,
+        "motor":                                  454,
+        "museos":                                 250,
+        "ocio_diurno":                            152, //parques temáticos y atracciones
+        "ocio_y_playas":                          454,
+        "oficinas_de_informacion":                145,
+        "otras_especialidades":                   454,
+        "otros_centros_de_congresos_de_tenerife": 398,
+        "pastelerias":                            582,
+        "pensiones":                              25,
+        "por_mar_y_aire":                         150,
+        "publicaciones_y_revistas":               522,
+        "restaurantes":                           30,
+        "salud_y_belleza":                        159,
+        "senderismo":                             327,
+        "situacion_orografia_y_vegetacion":       506,
+        "sol_y_playa":                            154,
+        "tenis_y_especialidades_con_raqueta":     454,
+        "touroperadores":                         251,
+        "transporte_publico":                     163,
+        "webcam":                                 342,
+        "zonas_de_acampada":                      18
+    },
+    'CODSUBAREAS':{
+        "arquitectura_tradicional":               278,
+        "biosphere":                              464,
+        "ciclismo":                               323,
+        "costumbres_y_folclore":                  155,
+        "especialidades_nauticas":                169,
+        "espectaculos":                           156,
+        "hipica":                                 379,
+        "motor":                                  268,
+        "museos":                                 155,
+        "ocio_y_playas":                          156,
+        "otras_especialidades":                   174,
+        "tenis_y_especialidades_con_raqueta":     175,
+        "zonas_de_acampada":                      157
+    },
+    'CODAREAS':{
+        "centros_culturales":                     6,
+        "instalaciones_deportivas":               15,
+        "ciclismo":                               15,
+        "especialidades_nauticas":                15,
+        "hipica":                                 15,
+        "motor":                                  15,
+        "otras_especialidades":                   15,
+        "pastelerias":                            null,
+        "tenis_y_especialidades_con_raqueta":     15
+    }
+});
+
