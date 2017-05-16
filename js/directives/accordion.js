9c75b4b3791bec36d1275f1059f3e30c99c7633a
Manlio García <quarkex@gmail.com>
2017-05-16 15:20:57 +0100

    refactored accordion to rely on nodes only

--- js/directives/accordion.js
+++ js/directives/accordion.js
@@ -1,21 +1,21 @@
 app.directive('appAccordion', function () {
 
-    var section = '<h2 ng-bind="translate( \'pagina.titulo_\', node.parent.title )"></h2>';
+    var section = '<h2 ng-bind="translate( \'pagina.titulo_\', node.parent.parent.title )"></h2>';
 
-    var header = '<accordion-heading>{{ translate(\'pagina.titulo_\', label) | gsub:\'_\':\' \' | capitalize }}</accordion-heading>';
+    var header = '<accordion-heading>{{ translate(\'pagina.titulo_\', link.title) | gsub:\'_\':\' \' | capitalize }}</accordion-heading>';
 
-    var lone_button = '{{ translate(\'pagina.titulo_\', label) | gsub:\'_\':\' \' | capitalize }}';
-    lone_button = '<li ng-class="{ \'active\': (params.id == label) }">' + lone_button + '</li>';
-    lone_button = '<a ng-if="! link.content" ng-href="{{ link.href.substr(0,1) == \'/\' ? \'#/\' + lang() : \'\' }}{{ link.href }}">' + lone_button + '</a>';
+    var lone_button = '{{ translate(\'pagina.titulo_\', link.title) | gsub:\'_\':\' \' | capitalize }}';
+    lone_button = '<li ng-class="{ \'active\': (current_section() == link.id) }">' + lone_button + '</li>';
+    lone_button = '<a ng-if="link.nodes.length == 0" ng-href="{{ link.href.substr(0,1) == \'/\' ? \'#/\' + lang() : \'\' }}{{ link.href }}">' + lone_button + '</a>';
 
-    var multiple_buttons = '{{ translate(\'pagina.titulo_\', sublabel) | gsub:\'_\':\' \' | capitalize }}';
-    multiple_buttons = '<li ng-class="{ \'active\': (params.id == sublabel) }">' + multiple_buttons + '</li>';
-    multiple_buttons = '<a ng-if="link.content" ng-href="{{ sublink.href.substr(0,1) == \'/\' ? \'#/\' + lang() : \'\' }}{{ sublink.href }}" ng-repeat="(sublabel, sublink) in link.content">' + multiple_buttons + '</a>';
+    var multiple_buttons = '{{ translate(\'pagina.titulo_\', sublink.title) | gsub:\'_\':\' \' | capitalize }}';
+    multiple_buttons = '<li ng-class="{ \'active\': (current_section() == sublink.id) }">' + multiple_buttons + '</li>';
+    multiple_buttons = '<a ng-if="link.content" ng-href="{{ sublink.href.substr(0,1) == \'/\' ? \'#/\' + lang() : \'\' }}{{ sublink.href }}" ng-repeat="sublink in link.nodes">' + multiple_buttons + '</a>';
 
     var template = lone_button + multiple_buttons;
     template = '<ul layout="column">' + template + '</ul>';
     template = header + template;
-    template = '<accordion-group is-open="::(node.title == label )" ng-repeat="(label, link) in sublinks">' + template + '</accordion-group>';
+    template = '<accordion-group is-open="::(node.parent.title == link.id )" ng-repeat="link in node.parent.parent.nodes">' + template + '</accordion-group>';
     template = '<accordion close-others="true" ng-controller="accordionCtrl">' + template + '</accordion>';
     template = '<div class="accordion-wrapper hide-for-print">' + section + template + '</div>';
 
