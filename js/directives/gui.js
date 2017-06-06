d579ccbaf980deddc65ec9c43d63a03a7d961c89
Manlio García <quarkex@gmail.com>
2017-06-06 12:26:46 +0100

    fixed typo in variable declaration

--- js/directives/gui.js
+++ js/directives/gui.js
@@ -1,7 +1,7 @@
 app.directive('appGui', function () {
 
     var template = 
-        '<div ng-init="showDiv = true;" ng-show="showDiv"  ng-if="isExplorer" bind-html-compile="translate(\'general.\', \'actualizarnavegador\')" ng-click="showDiv = !showDiv;" layout="column" layout-margin layout-padding></div>'+
+        '<div ng-init="showDiv = true;" ng-show="showDiv"  ng-if="isIE" bind-html-compile="translate(\'general.\', \'actualizarnavegador\')" ng-click="showDiv = !showDiv;" layout="column" layout-margin layout-padding></div>'+
         '<app-titlebar class="hide-for-print"></app-titlebar>' +
         '<app-breadcrumbs class="hide-for-print" ng-if=" path() != ( \'/\' + lang() )"></app-breadcrumbs>' +
         '<app-header class="hide-for-print"></app-header>' +
