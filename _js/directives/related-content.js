ceba4ab1b5a110a01a1d4c8c4df4f524db67eec5
Manlio García <quarkex@gmail.com>
2018-01-09 02:53:09 +0000

    adjusted related content presentation

--- _js/directives/related-content.js
+++ _js/directives/related-content.js
@@ -19,9 +19,11 @@ app.directive('appRelatedContent', function () {
     content_b = '<div flex>' + content_b + '</div>';
 
     content = '<md-card-title style="padding: 16px;">' + content_a + content_b + '</md-card-title>';
-    content = '<md-card flex md-theme="default" md-theme-watch style="min-height: 150px; background-color: #f6f6f6;">' + content + '</md-card>';
-    content = '<md-grid-tile md-rowspan="1" md-colspan="1" ng-repeat="card in element().CONTENIDOS_RELACIONADOS">' + content + '</md-grid-tile>';
-    content = '<md-grid-list md-cols-gt-xs="2" md-cols="1" md-row-height="3:1" flex class="hide-for-print">' + content + '</md-grid-list>';
+    content = '<md-card flex md-theme="default" md-theme-watch style="min-height: 150px; background-color: #f6f6f6;" ng-repeat="card in element().CONTENIDOS_RELACIONADOS">' + content + '</md-card>';
+
+    content = '<div layout="row" layout-wrap flex="100" layout-align="begin strech">' + content + '</div>';
+    //content = '<md-grid-tile md-rowspan="1" md-colspan="1" ng-repeat="card in element().CONTENIDOS_RELACIONADOS">' + content + '</md-grid-tile>';
+    //content = '<md-grid-list md-cols-gt-xs="2" md-cols="1" md-row-height="3:1" flex class="hide-for-print">' + content + '</md-grid-list>';
 
     var template = header + content;
     template = '<div layout-margin ng-if="element().CONTENIDOS_RELACIONADOS.length > 0">' + template + '</div>';
